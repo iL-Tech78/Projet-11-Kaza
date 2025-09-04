4683d75ef1b7991396f3e1059e72ff0117a866dd
IL-iliesse78 <86209795+IL-iliesse78@users.noreply.github.com>
2025-09-04 10:15:43 +0200

    Initialisation du projet Kasa avec Vite + React + Sass

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import data from './data/logements.json'
+
+function App() {
+  console.log(data) // pour vérifier dans la console navigateur
+  return <h1>Kasa</h1>
+}
+
+export default App
+
+
+
+
