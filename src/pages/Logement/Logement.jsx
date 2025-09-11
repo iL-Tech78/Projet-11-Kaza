20646f0eb944cd232fa9aa36e0ffc5b5fe9225a1
IL-iliesse78 <86209795+IL-iliesse78@users.noreply.github.com>
2025-09-11 10:08:31 +0200

    création de la page 404 en responsive et gestion de  l’affichage pour toute route inconnue et pour les id de logement invalides

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,14 +1,26 @@
 import { useParams } from 'react-router-dom'
+import logements from '@/data/logements.json'
+import NotFound from '@/pages/NotFound/NotFound.jsx'
 import './Logement.scss'
 
 export default function Logement() {
   const { id } = useParams()
-  document.title = 'Kasa — Détail logement'
+  const logement = logements.find((l) => l.id === id)
+
+  if (!logement) {
+    // Si id inconnu -Z page 404
+    return <NotFound />
+  }
+
+  document.title = `Kasa — ${logement.title}`
 
   return (
     <section className="logement" aria-labelledby="logement-title">
-      <h1 id="logement-title">Logement #{id}</h1>
-      <p>Go mettre les détails ici</p>
+      <div className="container">
+        <h1 id="logement-title" className="sr-only">{logement.title}</h1>
+        {}
+        <p>Go mettre les détails ici</p>
+      </div>
     </section>
   )
 }
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -5,9 +5,15 @@ export default function NotFound() {
   document.title = 'Kasa — 404'
   return (
     <section className="notfound" aria-labelledby="nf-title">
-      <h1 id="nf-title">404 — Page non trouvée</h1>
-      <p>La page demandée n’existe pas.</p>
-      <p><Link to="/">Revenir à l’accueil</Link></p>
+      <div className="container notfound__inner">
+        <h1 id="nf-title" className="notfound__code">404</h1>
+        <p className="notfound__msg">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <p className="notfound__back">
+          <Link to="/">Retourner sur la page d’accueil</Link>
+        </p>
+      </div>
     </section>
   )
 }
